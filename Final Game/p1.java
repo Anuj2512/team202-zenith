dd79ab2c88b744385a1e6b2c548bbd535322b1b4
panchaltej <tejaspanchal49@gmail.com>
2018-04-13 07:07:50 -0700

    Added burger element

---
+++ Final Game/p1.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class p1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class p1 extends Elements
+{
+    /**
+     * Act - do whatever the p1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public p1(int x, int y){
+        super();
+        posX = x;
+        posY = y;
+    }   
+}
