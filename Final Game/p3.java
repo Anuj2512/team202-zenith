6fd55c78403f0450a28b8f46d40df730f6d67a8b
KiratiBhuva <35611972+KiratiBhuva@users.noreply.github.com>
2018-04-14 10:12:53 -0700

    pizza 3 handling

---
+++ Final Game/p3.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class p3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class p3 extends Elements
+{
+    /**
+     * Act - do whatever the p3 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public p3(int x, int y){
+        super();
+        posX = x;
+        posY = y;
+    } 
+}
