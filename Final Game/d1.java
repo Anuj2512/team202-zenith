46f771c2b04e3361f40c19e5bafa64a7dc98e0e6
Anuj Chaudhari <chaudharianuj93@gmail.com>
2018-04-17 18:10:47 -0700

    add d2

---
+++ Final Game/d1.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class d1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class d1 extends Elements
+{
+    /**
+     * Act - do whatever the d1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public d1(int x, int y){
+        super();
+        posX = x;
+        posY = y;
+    } 
+}
---
+++ Final Game/d2.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class d2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class d2 extends Elements
+{
+    /**
+     * Act - do whatever the d2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public d2(int x, int y){
+        super();
+        posX = x;
+        posY = y;
+    }   
+}
