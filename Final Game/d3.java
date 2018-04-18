cb9a4761d232ef44e7a81c9c0a69ade36fdc030d
panchaltej <tejaspanchal49@gmail.com>
2018-04-18 09:09:38 -0700

    Added 3rd drink object

---
+++ Final Game/d3.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class d3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class d3 extends Elements
+{
+    /**
+     * Act - do whatever the d3 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public d3(int x, int y){
+        super();
+        posX = x;
+        posY = y;
+    } 
+}
