7f685cd9416790a0f089897c8a657ff095747859
JainulP <jainul.patel@sjsu.edu>
2018-04-18 20:34:45 -0700

    added code for fries 1

---
+++ Final Game/f1.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class f1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class f1 extends Elements
+{
+    /**
+     * Act - do whatever the f1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public f1(int x, int y){
+        super();
+        posX = x;
+        posY = y;
+    } 
+}
