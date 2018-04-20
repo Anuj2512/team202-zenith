e555eb44732eea5fb65c9fb7ca3d591ef4735367
arpitm91 <32207496+arpitm91@users.noreply.github.com>
2018-04-19 21:36:58 -0700

    f3

---
+++ Final Game/f3.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class f3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class f3 extends Elements
+{
+    /**
+     * Act - do whatever the f3 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public f3(int x, int y){
+        super();
+        posX = x;
+        posY = y;
+    }    
+}
