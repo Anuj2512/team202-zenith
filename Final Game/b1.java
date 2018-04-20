290b0a091b42100ab7d057ba2b4c41008483436d
Anuj Chaudhari <chaudharianuj93@gmail.com>
2018-04-20 08:18:14 -0700

    add burger type 1

---
+++ Final Game/b1.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class b1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class b1 extends Elements
+{
+    /**
+     * Act - do whatever the b1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public b1(int x, int y){
+        super();
+        posX = x;
+        posY = y;
+    }   
+}
