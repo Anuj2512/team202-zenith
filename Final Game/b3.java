fede2d509a07e05a02cb64f4bc72f6391383b877
JainulP <jainul.patel@sjsu.edu>
2018-04-21 08:42:46 -0700

    added burger 3

---
+++ Final Game/b3.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class b3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class b3 extends Elements
+{
+    /**
+     * Act - do whatever the b3 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public b3(int x, int y){
+        super();
+        posX = x;
+        posY = y;
+    }  
+}
