fd0de1188c2c9c309eff06b719485bdd86342b80
panchaltej <tejaspanchal49@gmail.com>
2018-04-20 17:18:48 -0700

    Added 2nd burger object

---
+++ Final Game/b2.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class b2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class b2 extends Elements
+{
+    /**
+     * Act - do whatever the b2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public b2(int x, int y){
+        super();
+        posX = x;
+        posY = y;
+    }   
+}
