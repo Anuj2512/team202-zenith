6418846b7f4962603987ee88ddc0909e9c675817
Anuj Chaudhari <chaudharianuj93@gmail.com>
2018-04-25 06:03:43 -0700

    add box class

---
+++ Final Game/Box.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Box here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Box extends Actor
+{
+    public Box(){
+        GreenfootImage image = getImage();
+        int scale = Math.max(image.getWidth(),image.getHeight());
+        image.scale(image.getWidth()*100/scale, image.getHeight()*100/scale);
+        setImage(image);
+    }
+    /**
+     * Act - do whatever the Box wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
